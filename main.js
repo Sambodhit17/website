bb04f961cdb7b455727ca373e5b64541a499e6b9
Sambodhit17 <92640348+Sambodhit17@users.noreply.github.com>
2021-10-16 20:58:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function function1() {
+    document.getElementById("audio1").onplay();
+
+    
+}
+
+function function2() {
+    document.getElementById("audio2").onplay();
+    
+}
+
+function function3() {
+    document.getElementById("audio3").onplay();
+    
+}
+
+function function4() {
+    document.getElementById("audio4").onplay();
+
+    
+}
+
+function function5() {
+    document.getElementById("audio5").onplay();
+    
+}
+   
+    
+
+    
+
+    
